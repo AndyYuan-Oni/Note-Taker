56f8841bb3619765f3afc3341281e44b91f879c0
Andy <andyyuan2017@gmail.com>
2020-02-17 04:22:14 +1100

    heroku change

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var data = fs.readFileSync("./db/db.json", "utf8");
 var jsonDB = JSON.parse(data);
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
